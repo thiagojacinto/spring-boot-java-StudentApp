1d10e3b7011d03e633d70ef1d6e3f028c527a99a
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2019-11-14 21:29:44 -0300

    POST working without Errors

--- web/script.js
+++ web/script.js
@@ -3,18 +3,25 @@
 // ERROR verifying function
 async function CheckError(response) {
   if (response.status >= 200 && response.status <= 299) {
-    return response.json();
+    return response;
   } else {
     throw Error(response.statusText);
   }
 }
+
+// Common HEADERS
+// const header = new Headers();
 
 // GET Method function
 async function getData(url = '') { //, data = {}) {
   // Default options are marked with *
   const response = await fetch(url, {
     method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    mode: 'same-origin', // no-cors, *cors, same-origin
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'default',
+    headers: {
+      'Content-Type': 'application/json'
+    }
     /*
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     credentials: 'omit', // include, *same-origin, omit
@@ -25,28 +32,35 @@ async function getData(url = '') { //, data = {}) {
     redirect: 'follow', // manual, *follow, error
     referrer: 'no-referrer', // no-referrer, *client
     */
-  }).then(CheckError);
+  }).then(response => CheckError(response)); // .then(CheckError(response));
+  return JSON.stringify(response);
   // return await response.json(); // parses JSON response into native JavaScript objects // put it inside 'CheckError' function
 }
 
-// Tries GET
-try {
-  const dataFromGet = getData('http://localhost:8080/students');
-  console.log(JSON.stringify(dataFromGet)); // JSON-string from `response.json()` call
-} catch (error) {
-  console.error(error);
+function tryToGet() {
+  // Tries GET
+  try {
+    const dataFromGet = getData('http://localhost:8080/students/');
+    console.log(JSON.stringify(dataFromGet)); // JSON-string from `response.json()` call
+    return JSON.stringify(dataFromGet);
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+const getButton = document.querySelector('.get1-button');
+getButton.addEventListener('click', e => {
+  // console.log(e);  // Verify event
+  e.preventDefault();
+  tryToGet();
+});
 
 // ***************** POST *****************
+// Important 
+const methodUrl = 'http://localhost:8080/students/';
+// const methodArgs = document.querySelector('.post-text').value;
 
-const postButton = document.querySelector('.post-button');
-let methodUrl = 'http://localhost:8080/students/';
-let methodArgs = {
-  "id": 22,
-  "name": "Thiago Test Post 3",
-  "course": "JavaScript dev"
-}
-postButton.addEventListener('click', triesPost);
+
 
 // POST Method function
 async function postData(url = '', data = {}) {
@@ -63,19 +77,28 @@ async function postData(url = '', data = {}) {
     redirect: 'follow', // manual, *follow, error
     referrer: 'no-referrer', // no-referrer, *client
     body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return await response.json(); // parses JSON response into native JavaScript objects
+  }).then(response => CheckError(response));
+  return JSON.stringify(response); // parses JSON response into native JavaScript objects
 }
 
 // Tries POST
-function triesPost() {
+function tryToPost(methodArgs) {
   try {
-    const dataFromPost = postData(methodUrl, methodArgs);
+    const dataFromPost = postData(methodUrl, JSON.parse(methodArgs));
     console.log(JSON.stringify(dataFromPost)); // JSON-string from `response.json()` call
   } catch(error) {
     console.error(error);
   }
 }
+
+const postButton = document.querySelector('.post-button');
+postButton.addEventListener('click', e => {
+  e.preventDefault();   // Block auto-refres
+  const methodArgs = document.querySelector('.post-text').value;
+  tryToPost(methodArgs);
+});
+
+// ***************** DELETE *****************
 
 // DELETE Method function
 async function deleteData(url = '') { //, data = {}) {
@@ -92,6 +115,6 @@ async function deleteData(url = '') { //, data = {}) {
     redirect: 'follow', // manual, *follow, error
     referrer: 'no-referrer', // no-referrer, *client
     body: '' //JSON.stringify(data) // body data type must match "Content-Type" header
-  });
+  }).then(CheckError);
   return await response.json(); // parses JSON response into native JavaScript objects
 }
